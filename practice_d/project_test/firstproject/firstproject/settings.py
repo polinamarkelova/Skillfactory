abc0fde362ad3ad7430e0f63d74c287b262acc7a
polinamarkelova <164.39polina@gmail.com>
2022-08-17 21:16:37 +0200

    added new flatpages and fixed bugs

--- practice_d/project_test/firstproject/firstproject/settings.py
+++ practice_d/project_test/firstproject/firstproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'fpages'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
